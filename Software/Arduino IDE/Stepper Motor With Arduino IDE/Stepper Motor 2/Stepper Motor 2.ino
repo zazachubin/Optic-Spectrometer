50f280999569f08599086438e4d0498674778471
Zaza Chubinidze <zazachubin@gmail.com>
2022-03-19 15:26:46 +0400

    Create project

---
+++ Software/Arduino IDE/Stepper Motor With Arduino IDE/Stepper Motor 2/Stepper Motor 2.ino
@@ -0,0 +1,32 @@
+void setup() {
+  // initialize digital pin 13 as an output.
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW); 
+  digitalWrite(10, LOW); 
+  digitalWrite(11, LOW);
+  delay(10);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH); 
+  digitalWrite(10, LOW); 
+  digitalWrite(11, LOW);
+  delay(10);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW); 
+  digitalWrite(10, HIGH); 
+  digitalWrite(11, LOW);
+  delay(10);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW); 
+  digitalWrite(10, LOW); 
+  digitalWrite(11, HIGH);
+  delay(10);
+}
+
